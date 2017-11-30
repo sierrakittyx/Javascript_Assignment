836e2114d76e12ae5ce360e9c056150507665bd8
sierrakittyx <sierrab373@gmail.com>
2017-11-30 16:32:22 -0600

    finished assignment locally

---
+++ sb48236.js
@@ -0,0 +1,6 @@
+// Rename this file to a file called <<EID>>.js (e.g. jr007.js)
+// Write a line of code to get an element called "span1"
+// Upload your solution by the deadline
+
+
+var span1 = document.getElementById("span1");
